e0f4eb915021291165b6136060487d9485a48050
gan peng <917006820@qq.com>
2025-07-03 13:52:23 +0800

    示例

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,7 +80,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Examples
+          示例
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
