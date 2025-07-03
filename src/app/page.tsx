0f11764cc4478e686ebbb31daa9fb9eb8f2f6b3d
gan peng <917006820@qq.com>
2025-07-03 13:56:38 +0800

    跳转

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          跳转到 nextjs.org →
         </a>
       </footer>
     </div>
